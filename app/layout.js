e5057e6487ce237b1e2fcc3afb78f28f06d7a864
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2023-11-03 18:51:34 -0500

    Fixed problem

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import {config} from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false;
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link href="https://api.fontshare.com/v2/css?f[]=zodiak@401&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap" rel="stylesheet"/>
+      </Head>
       <body className={inter.className}>
         <Navbar/>
         {children}
